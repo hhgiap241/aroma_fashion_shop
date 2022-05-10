57194d24dfcc15754d0bab36547aa49e7cb5566f
giaphoang <hhgiap241>
2022-05-10 12:01:15 +0700

    get mapping for all pages

--- src/main/java/com/kms/hhgiap/aroma/MainController.java
+++ src/main/java/com/kms/hhgiap/aroma/MainController.java
@@ -13,4 +13,40 @@ public class MainController {
     public String showCategoryPage() {
         return "category";
     }
+    @GetMapping("/single-product")
+    public String showProductDetailPage() {
+        return "single-product";
+    }
+    @GetMapping("/checkout")
+    public String showCheckoutPage() {
+        return "checkout";
+    }
+    @GetMapping("/single-order")
+    public String showOrderDetailPage() {
+        return "single-order";
+    }
+    @GetMapping("/cart")
+    public String showCartPage() {
+        return "cart";
+    }
+    @GetMapping("/blogs")
+    public String showBlogListPage() {
+        return "blog";
+    }
+    @GetMapping("/single-blog")
+    public String showBlogDetailPage() {
+        return "single-blog";
+    }
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+    @GetMapping("/contact")
+    public String showContactPage() {
+        return "contact";
+    }
 }
